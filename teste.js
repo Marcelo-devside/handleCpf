c162b3f558aa99748d170863d6e1979785489dda
Marcelo <maxcelu@yahoo.com.br>
2021-11-24 21:50:36 -0300

    version 1.0 final

--- teste.js
+++ teste.js
@@ -1,63 +1,80 @@
-// function  calculoFinal (resultado) {
-//     resultado = resultado % 11;  
-//     if (resultado != 0 && resultado != 1) {
-//         resultado = 11 - resultado 
-//     } else {
-//         resultado = 0
-//             }
-//     console.log(resultado)
-// }
-// calculoFinal(13)
-
-//     clickAddNamber()
-// function multiNamber(digitos){
-    //     for (i = 10 ; i > 1; i-- ) {
-        //         formula = i * digitos
-        //     }
-        //     return formula
-    // }
-    
-//     function somaTudo(digitos){
-    //         let total = 0
-//         for (let num of digitos){
-    //         total = total + num
-//     }
-//     return total
-// }
-
-// let arrayNumeros = [3, 5, 2]
-// let resulta = multiNamber(arrayNumeros)
-// console.log(resulta)
-
-// let arrayNumeros = [3, 5, 2]
-// let resulta = somaTudo(arrayNumeros)
-// console.log(resulta)
-
-// function clickStateLocal(){ 
-//     let capitura = document.querySelector("#localname")
-//     console.log(capitura.value)
-// }
-function tcPress(event){
-    let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-    const novoArray = []
-    if ( event.key in numbers ){
+function Init() {
+    const entr = document.querySelector("#oito_Numeros2");
+    entr.addEventListener("keypress", checkInput, false);
+    document.querySelector(".cEst").focus();
+}
+window.addEventListener("load", Init);
+function checkInput(e) {
+    const char = String.fromCharCode(e.keyCode);
+    const pattern = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    if (char in pattern) {
+        return true;
     }
-    else {
-        alert("Digite apenas Números")
-        window.location.reload()
+    e.preventDefault();
+    alert(`Você digitou \n\n " ${char} " \n\n Digite somente números!`);
+}
+function btnSearh(option1, option2) {
+    const btnSearhSelect = document.querySelector("#BtnPesquisa");
+    const btnSearhGerador = document.querySelector("#maker");
+    btnSearhGerador.style.visibility = option1;
+    btnSearhSelect.style.visibility = option2;
+}
+function validationOfSize() {
+    const typingNamber = document.querySelector(".oito_Numeros").value;
+    if (typingNamber.length != 8) {
+        btnSearh("visible", "hidden");
+        return alert("Você não digitou os oito números!");
     }
 }
-
-window.addEventListener("keypress", tcPress)
-
-function clickAddNamber(){
-    let capitura = document.querySelector('.oito_Numeros')
-    let capituras = capitura.value;
-    let capitura2 = document.querySelector('.cEst')
-    let capituras2 = capituras + capitura2.value
-    let ref =document.querySelector('.res')
-    ref.innerText = capituras2
-    let numArray= Array.from(capituras2)
-    const novoArray = numArray.map(numero => parseInt(numero))
-    return console.log(novoArray)
-    }
+function clickAddNamber() {
+    btnSearh("hidden", "visible");
+    validationOfSize();
+    const typingNamber = document.querySelector(".oito_Numeros").value;
+    const typingEstate = document.querySelector(".cEst").value;
+    const showScream = document.querySelector(".res");
+    const handleNumber = typingNamber + typingEstate;
+    let noveNumeros = handleNumber.substring(0, 9);
+    const handleNumberTen = () => {
+        let soma = 0;
+        for (let i = 10; i > 1; i--) {
+            soma += noveNumeros.charAt(10 - i) * i;
+        }
+        let resultados = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+        return resultados;
+    };
+    const handleNumber2 = handleNumber + handleNumberTen();
+    const handleNumberEleven = () => {
+        let somas = 0;
+        noveNumeros = handleNumber2.substring(0, 10);
+        for (let k = 11; k > 1; k--) {
+            somas += noveNumeros.charAt(11 - k) * k;
+        }
+        let resultado = somas % 11 < 2 ? 0 : 11 - (somas % 11);
+        return resultado;
+    };
+    const handleNumber3 = handleNumber2 + handleNumberEleven();
+    const resultExitMask = () => {
+        showScream.textContent = handleNumber3.replace(
+            /^(\d{3})+(\d{3})+(\d{3})/,
+            "$1.$2.$3-"
+        );
+    };
+    resultExitMask();
+}
+const areaInputMask = () => {
+    const araseResult = document.querySelector(".res");
+    const initalValue = "00000000000";
+    araseResult.innerText = initalValue.replace(
+        /^(\d{3})+(\d{3})+(\d{3})/,
+        "$1.$2.$3-"
+    );
+};
+function areaInput() {
+    btnSearh("visible", "hidden");
+    const araseInput = document.querySelector("#oito_Numeros2");
+    const defaultEst = document.querySelector(".cEst");
+    araseInput.value = "";
+    areaInputMask();
+    defaultEst.value = "7";
+    Init();
+}
