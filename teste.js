5d991fe08b0fc63b24e9439cf1a383c937dd17e0
Marcelo <maxcelu@yahoo.com.br>
2021-12-03 12:00:35 -0300

    fix version  mobile

--- teste.js
+++ teste.js
@@ -19,6 +19,8 @@ const operationDisable = () =>{
 }
 function Init() {
     document.querySelector('.marcar').hidden = true
+    document.querySelector("#BtnPesquisaMobile").style.display = "none"
+    document.querySelector("#BtnMakerMobile").style.display = "none"
     const entr = document.querySelector("#oito_Numeros2");
     entr.addEventListener("keypress", checkInput, false);
     // document.querySelector(".cEst").focus();
@@ -110,4 +112,86 @@ function areaInput() {
     araseInput.value= null;
     defaultEst.value = "7";
     Init();
+}
+// -----------------------------------------------------------------------------
+// mobile
+function btnSearh2(optionMobile1, optionMobile2, optionMobile3) {
+    const BtnStartMobile = document.querySelector("#BtnStartMobile");
+    const btnSearhSelectMobile = document.querySelector("#BtnPesquisaMobile");
+    const btnSearhGeradorMobile = document.querySelector("#BtnMakerMobile");
+    btnSearhGeradorMobile.style.display = optionMobile1;
+    btnSearhSelectMobile.style.display = optionMobile2;
+    BtnStartMobile.style.display = optionMobile3;
+}
+const BtnStartMobile = document.querySelector("#BtnStartMobile");
+const disableImage = document.querySelector("#addSaida");
+const dataInput1 = document.querySelector(".fildEst");
+const dataInput2 = document.querySelector(".cpf");
+BtnStartMobile.addEventListener('click', function inputOfNumerMobile() {
+    btnSearh2("initial", "none", "none")
+    disableImage.style.visibility = "hidden";
+    dataInput1.style.visibility = "visible";
+    dataInput2.style.visibility = "visible";
+})
+function validationOfSize2() {
+    const typingNamber = document.querySelector(".oito_Numeros").value;
+    if (typingNamber.length != 8) {
+        // Init()
+        popupWrapper.style.display = 'block'
+        paragrafo.textContent ="Você não digitou os oito números!";
+        btnSearh2("initial", "none", "none");
+        disableImage.style.visibility = "hidden";
+        dataInput1.style.visibility = "visible";
+        dataInput2.style.visibility = "visible";
+        return numberExit.preventDefault()
+    }
+}
+function clickAddNamber2() {
+    btnSearh2("none", "initial", "none");
+    disableImage.style.visibility = "visible";
+    dataInput1.style.visibility = "hidden";
+    dataInput2.style.visibility = "hidden";
+    validationOfSize2();
+    const typingNamber = document.querySelector(".oito_Numeros").value;
+    const typingEstate = document.querySelector(".cEst").value;
+    const showScream = document.querySelector(".res");
+    const handleNumber = typingNamber.concat(typingEstate);
+    let nineNumber = handleNumber.substring(0, 9);
+    const handleNumberTen = () => {
+        let soma = 0;
+        for (let i = 10; i > 1; i--) {
+            soma += nineNumber.charAt(10 - i) * i;
+        }
+        let digitTen = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+        return digitTen;
+    };
+    const handleNumber2 = handleNumber + handleNumberTen();
+    const handleNumberEleven = () => {
+        let somas = 0;
+        nineNumber = handleNumber2.substring(0, 10);
+        for (let k = 11; k > 1; k--) {
+            somas += nineNumber.charAt(11 - k) * k;
+        }
+        let digitEleven = somas % 11 < 2 ? 0 : 11 - (somas % 11);
+        return digitEleven;
+    };
+    const handleNumber3 = handleNumber2 + handleNumberEleven();
+    const resultExitMask = () => {
+        showScream.textContent = handleNumber3.replace(
+            /^(\d{3})+(\d{3})+(\d{3})/,
+            "$1.$2.$3-"
+        );
+    };
+    resultExitMask();
+}
+function areaInput2() {
+    btnSearh2("initial", "none", "none");
+    const araseInput = document.querySelector("#oito_Numeros2");
+    const defaultEst = document.querySelector(".cEst");
+    araseInput.value= null;
+    defaultEst.value = "7";
+    disableImage.style.visibility = "hidden";
+    dataInput1.style.visibility = "visible";
+    dataInput2.style.visibility = "visible";
+    // Init();
 }
